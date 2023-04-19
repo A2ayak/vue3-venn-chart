74adb9d3753618ac5dbbad7f161a622b0dd0eed6
A2ayak <429853312@qq.com>
2023-04-19 15:55:52 +0800

    feat：新增css打包进lib的js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), VueSetupExtend()],
+	plugins: [vue(), VueSetupExtend(), cssInjectedByJsPlugin()],
 	build: {
 		outDir: 'lib',
 		lib: {
