4da23593ddaac227d5675d18960f3291aa13128a
A2ayak <429853312@qq.com>
2023-04-19 15:57:25 +0800

    refactor：代码优化调整

--- example/App.vue
+++ example/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import VennChart from '../packages/VennChart.vue'
+import VennChart from '../packages'
 </script>
 
 <template>
--- packages/index.ts
+++ packages/index.ts
@@ -1,14 +1,11 @@
 import VennChart from './VennChart.vue'
-import type { App } from 'vue'
+import { App } from 'vue'
 
 // import module
 export { VennChart }
-console.log(VennChart.name)
 
 // vue.use
-const install = (app: App) => {
+VennChart.install = (app: App) => {
 	app.component(VennChart.name, VennChart)
 }
-export default {
-	install
-}
+export default VennChart
