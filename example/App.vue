5d78dcf68faa5b72f7bfec2bfa564496ad6f2ab2
A2ayak <429853312@qq.com>
2023-04-20 10:10:31 +0800

    refactor：update the demo

--- example/App.vue
+++ example/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import VennChart from '../packages'
+
+const chartData = ref([
+	{ sets: ['A'], label: '12', size: 12 },
+	{ sets: ['B'], label: '8', size: 8 },
+	{ sets: ['A', 'B'], label: '4', size: 4 }
+])
+const colors = ['#ee4035', '#0392cf', '#7bc043']
+const legend = ['left(左边)', 'right(右边)', 'common(同时存在)']
 </script>
 
 <template>
 	<div class="chart-container">
-		<VennChart />
+		<VennChart :data="chartData" :colors="colors" :legend="legend" />
 	</div>
 </template>
 
